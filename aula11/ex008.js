bbe096b466821395e7e4945c12230dd52925f4c1
silvaozias <oziasdosanjosilva@gmail.com>
2024-04-09 09:09:22 -0300

    aTUALIZEI EXERCICIOS

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 61;
+console.log(`a velocidade do seu carro é ${vel} km/h`);
+if (vel > 60) {
+        console.log(`Você ultrapassou a velocidade permitida.  MULTADO!`)
+    }
+console.log(`Dirija sempre utilizando o sinto de segunrança!`)
